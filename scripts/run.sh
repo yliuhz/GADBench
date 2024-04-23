09abda9a0f8938a440bbaa4e2a681b3d1e78bf18
yliuhz <yliuhz@zju.edu.cn>
2024-04-23 19:26:17 +0800

    commit from ust4090-1

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,6 +2,11 @@
 
 cd ..
 
-CUDA_VISIBLE_DEVICES=2 python -u benchmark.py --datasets 0-8 --trials 3 --semi_supervised 1 2>&1 | tee logs/benchmark0-8.log
+
+## semi-supervised
+# CUDA_VISIBLE_DEVICES=2 python -u benchmark.py --datasets 0-8 --trials 3 --semi_supervised 1 2>&1 | tee logs/benchmark0-8.log
+
+## fully-supervised
+CUDA_VISIBLE_DEVICES=2 python -u benchmark.py --datasets 0-9 --trials 3 --semi_supervised 0 2>&1 | tee logs/benchmark0-9.log
 
 cd -
